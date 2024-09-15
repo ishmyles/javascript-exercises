3622fb5dc3ff36553cd61b9b1fdb45d4b0d6863c
Myles Gaerlan <ish.myles@gmail.com>
2024-09-15 22:00:21 +1000

    Fixed removeFromArray function
    Function now removes from a list of values (Passing all tests)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removeList) {
+  const notInRemoveList = (num) => !removeList.includes(num);
+  return array.filter(notInRemoveList);
 };
 
 // Do not edit below this line
