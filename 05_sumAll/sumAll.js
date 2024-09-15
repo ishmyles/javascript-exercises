cb0fec1b3131b4c4e2f305b3ae861df0d9aab6c5
Myles Gaerlan <ish.myles@gmail.com>
2024-09-15 22:17:42 +1000

    Fixed sumAll function, now passing tests
    Function is now able to catch errors & sum numbers regardless of input order

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  // Catches invalid inputs such as negative values, decimals & non-numerical values
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    num1 % 1 > 0 ||
+    num2 % 1 > 0
+  )
+    return "ERROR";
 
+  return num1 < num2
+    ? sumNumbersBetween(num1, num2)
+    : sumNumbersBetween(num2, num1);
 };
+
+function sumNumbersBetween(minNumber, maxNumber) {
+  let total = 0;
+  let currentNumber = minNumber;
+  while (currentNumber <= maxNumber) {
+    total += currentNumber;
+    currentNumber++;
+  }
+  return total;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
