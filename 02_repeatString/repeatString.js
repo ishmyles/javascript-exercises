f0d3ccbd0046f384d88a8f69eb49b4dd695c6636
Myles Gaerlan <ish.myles@gmail.com>
2024-09-15 21:05:10 +1000

    Completed function repeatString function
    This function doesnt use String.repeat() and passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num < 0) return "ERROR";
+  let concatinatedString = "";
 
+  while (num) {
+    concatinatedString += string;
+    num--;
+  }
+  return concatinatedString;
 };
 
 // Do not edit below this line
