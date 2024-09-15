2890639074b24fe9cbd773edab98941ed6fce263
Myles Gaerlan <ish.myles@gmail.com>
2024-09-15 22:43:14 +1000

    Fixed both convertToCelcius & convertToFahrenheit functions
    Both funcions can handle negative numbers and rounds to 1 decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitTemp) {
+  const celsiusTempString = (((fahrenheitTemp - 32) * 5) / 9).toFixed(1);
+  return Number(celsiusTempString);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celciusTemp) {
+  const fahrenheitTempString = ((celciusTemp * 9) / 5 + 32).toFixed(1);
+  return Number(fahrenheitTempString);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
