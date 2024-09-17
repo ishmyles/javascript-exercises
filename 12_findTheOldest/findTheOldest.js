3e997e3de364f5ff9c335f8ba24e7ec407e930c3
Myles Gaerlan <ish.myles@gmail.com>
2024-09-17 15:54:20 +1000

    Completed findTheOldest function
    User can input an array of people objects & return the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (peopleArr) {
+  const getAge = (person) => {
+    if (person.hasOwnProperty("yearOfDeath")) {
+      return person.yearOfDeath - person.yearOfBirth;
+    } else {
+      return new Date(Date.now()).getFullYear() - person.yearOfBirth;
+    }
+  };
+  const oldestPerson = peopleArr.reduce(
+    (oldestPerson, currentPerson) => {
+      currentPerson.age = getAge(currentPerson);
+      return currentPerson.age > oldestPerson.age
+        ? currentPerson
+        : oldestPerson;
+    },
+    { age: 0 }
+  );
+  delete oldestPerson.age;
+  return oldestPerson;
 };
 
 // Do not edit below this line
