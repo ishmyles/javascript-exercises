3d501c4d1b201a9bef167e685e1bc178efa75ab6
Myles Gaerlan <ish.myles@gmail.com>
2024-09-17 14:25:47 +1000

    Completed palndrome checker functionality
    Users are now able to input a string & the function works as expected

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const formattedString = string.toLowerCase().split(/[\W]/).join("");
+  const reverseString = formattedString.split("").reverse().join("");
+  return formattedString === reverseString;
 };
 
 // Do not edit below this line
